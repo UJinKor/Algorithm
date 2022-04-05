4ab5330fb428a07f8b55ed8b147e3943a656815a
홍유진 <0312jj@naver.com>
2022-04-05 23:44:02 +0900

    0405

--- 0405/src/Main_9095_123더하기.java
+++ 0405/src/Main_9095_123더하기.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main_9095_123더하기 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
