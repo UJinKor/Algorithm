2b512e94bb66c09a7c48e3f4161f3e3b9f54e4b0
홍유진 <0312jj@naver.com>
2022-04-08 18:16:32 +0900

    0408.

--- 0407/src/Main_11052_카드_구매하기.java
+++ 0407/src/Main_11052_카드_구매하기.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_11052_카드_구매하기 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
---
+++ 0407/src/Main_11052_카드_구매하기2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11052_카드_구매하기2 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N+1];
+		int[] cards = new int[N+1];
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		for (int i = 1; i <= N; i++) {
+			cards[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp[1] = cards[1];
+		for (int i = 2; i <= N; i++) {
+			int min = cards[i];
+			for (int j = 1; j < i; j++) {
+				min = Math.min(min,dp[i-j]+cards[j]);
+			}
+			dp[i] = min;
+		}
+		System.out.println(dp[N]);
+	}
+
+}
