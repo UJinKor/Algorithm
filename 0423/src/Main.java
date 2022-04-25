6eeb67a202d11603293a8edfe9031ef762e6d265
홍유진 <0312jj@naver.com>
2022-04-25 23:22:52 +0900

    0425

--- 0423/src/Main.java
+++ 0423/src/Main.java
@@ -5,11 +5,14 @@ import java.io.InputStreamReader;
 public class Main {
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		System.out.println("       _.-;;-._");
-		System.out.println("'-..-'|   ||   |");
-		System.out.println("'-..-'|_.-;;-._|");
-		System.out.println("'-..-'|   ||   |");
-		System.out.println("'-..-'|_.-''-._|");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		for (int i = 0; i < 4; i++) {
+			sum+= Integer.parseInt(br.readLine());
+		}
+		
+		System.out.println(sum/60);
+		System.out.println(sum%60);
 	}
 
 }
