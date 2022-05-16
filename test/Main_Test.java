2b156207ebd4477117cfa780054c50b50f3ac8ec
홍유진 <0312jj@naver.com>
2022-05-16 19:53:09 +0900

    0516

--- test/Main_Test.java
+++ test/Main_Test.java
@@ -1,16 +1,12 @@
+import java.util.Scanner;
+
 public class Main_Test {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("     /~\\").append("\n");
-        sb.append("    ( oo|").append("\n");
-        sb.append("    _\\=/_").append("\n");
-        sb.append("   /  _  \\").append("\n");
-        sb.append("  //|/.\\|\\\\").append("\n");
-        sb.append(" ||  \\ /  ||").append("\n");
-        sb.append("============").append("\n");
-        sb.append("|          |").append("\n");
-        sb.append("|          |").append("\n");
-        sb.append("|          |");
-        System.out.println(sb);
+        Scanner sc = new Scanner(System.in);
+        int D = sc.nextInt();
+        int H = sc.nextInt();
+        int W = sc.nextInt();
+        double a = D/Math.sqrt(((H*H)+(W*W)));
+        System.out.println((int)(a*H)+" "+(int)(a*W));
     }
 }
